41b3096b1f1e425aedd9687cdf08dcd3574aa7f6
krastiker <krastiker839@gmail.com>
2022-11-04 17:26:53 +0100

    1
    1

--- includes/connectDB.php
+++ includes/connectDB.php
@@ -6,5 +6,30 @@ if ($mysqli -> connect_errno) {
   echo "Failed to connect to MySQL: " . $mysqli -> connect_error;
   exit();
 }
+else{echo("CONNECTED DB <br/>");}
+ 
+
+$dataWanted="SELECT * FROM users";
+$res=$mysqli->query($dataWanted); 
+if(!$res) {
+  die('ERROR select from bd');}
+//  else echo('select from bd succes');
+//  var_dump($res);
+
+ $rows=$res->num_rows;
+   for($i=0; $i<$rows; ++$i)
+   {
+      $res->data_seek($i);
+      echo 'full name: '.$res->fetch_assoc()['full_name'].'<br/>';
+      echo 'login: '.$res->fetch_assoc()['login'].'<br/>';
+   }
+  // print_r($rows);
+
+
+
+
+
+
+
 
 ?>
